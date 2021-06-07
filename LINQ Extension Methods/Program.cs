c9bfa05f8eab199f33f1657e581b5f14188dd13d
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 16:24:32 +0530

    Add files via upload

---
+++ LINQ Extension Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            IEnumerable<int> EvenNumbers = Enumerable.Where(intList, n => n % 2 == 0);
+            foreach (int x in EvenNumbers)
+                Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
